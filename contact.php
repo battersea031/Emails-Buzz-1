1b1fb76280734f91e7752ee0fe1fda36f23e615f
battersea031 <battersea031@gmail.com>
2025-03-26 16:14:01 +0530

    function.php file changes

--- contact.php
+++ contact.php
@@ -1,4 +1,10 @@
 <?php get_header(); ?>
 
+<?php if (is_active_sidebar('contact_us_page')): ?>
+    <?php dynamic_sidebar('contact_us_page'); ?>
+<?php else: ?>
+    <!-- Time to add some widgets! -->
+    <p>Widgets no visible</p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -249,6 +249,14 @@ function themename_widgets_init(): void {
 		'before_title'  => '<h3 class="search_breadcrum">',
 		'after_title'   => '</h3>',
 	));
+	register_sidebar( array(
+		'name'          => __( 'Contact Us Page', 'contact_us_page' ),
+		'id'            => 'contact_us_page',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="contact_us_page">',
+		'after_title'   => '</h3>',
+	));
 }
 
 add_action( 'widgets_init', 'themename_widgets_init');
